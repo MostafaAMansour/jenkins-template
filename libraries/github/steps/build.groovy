67f83a11580f4874d070be725265d2542c339b06
Mostafa Mansour <85360001+MostafaAMansour@users.noreply.github.com>
2024-08-19 20:31:08 +0300

    updated

--- libraries/github/steps/build.groovy
+++ libraries/github/steps/build.groovy
@@ -2,14 +2,14 @@ def call(Map pipelineParams) {
     pipeline {
         agent any
 
-        parameters {
-            string(name: 'GITHUB_REPO', defaultValue: '', description: 'GitHub repository URL')
-            string(name: 'GITHUB_CREDENTIALS_ID', defaultValue: '', description: 'GitHub credentials ID')
-            string(name: 'BRANCH', defaultValue: 'main', description: 'Branch to clone')
-            string(name: 'DOCKER_IMAGE', defaultValue: '', description: 'Docker image name')
-            string(name: 'DOCKER_CREDENTIALS_ID', defaultValue: '', description: 'DockerHub credentials ID')
-            string(name: 'RECIPIENT_EMAIL', defaultValue: '', description: 'Recipient email address')
-        }
+        // parameters {
+        //     string(name: 'GITHUB_REPO', defaultValue: '', description: 'GitHub repository URL')
+        //     string(name: 'GITHUB_CREDENTIALS_ID', defaultValue: '', description: 'GitHub credentials ID')
+        //     string(name: 'BRANCH', defaultValue: 'main', description: 'Branch to clone')
+        //     string(name: 'DOCKER_IMAGE', defaultValue: '', description: 'Docker image name')
+        //     string(name: 'DOCKER_CREDENTIALS_ID', defaultValue: '', description: 'DockerHub credentials ID')
+        //     string(name: 'RECIPIENT_EMAIL', defaultValue: '', description: 'Recipient email address')
+        // }
         echo "hello world"
         // stages {
         //     stage('Clone Repository') {
