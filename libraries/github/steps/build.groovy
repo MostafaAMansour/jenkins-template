1b4eb85561cbb3c78f7e5124fb00fa76a84672c0
Mostafa Mansour <85360001+MostafaAMansour@users.noreply.github.com>
2024-08-19 21:03:59 +0300

    updated

--- libraries/github/steps/build.groovy
+++ libraries/github/steps/build.groovy
@@ -1,10 +1,21 @@
 def call(Map params) {
     node() {
         stage('Checkout') {
-        checkout([$class: 'GitSCM', 
-            branches: [[name: "${params.BRANCH}"]], 
-            userRemoteConfigs: [[url: "${params.GITHUB_REPO}", credentialsId: "${params.GITHUB_CREDENTIALS_ID}"]]
+            checkout([$class: 'GitSCM', 
+                branches: [[name: "${params.BRANCH}"]], 
+                userRemoteConfigs: [[url: "${params.GITHUB_REPO}", credentialsId: "${params.GITHUB_CREDENTIALS_ID}"]]
             ])
+        }
+        stage('Docker push') {
+            steps {
+                withCredentials([usernamePassword(credentialsId: "${params.DOCKER_CREDENTIALS_ID}", usernameVariable: 'USERNAME', passwordVariable: 'PASSWORD')]) {
+                    sh '''
+                    echo "\${PASSWORD}" | docker login -u "\${USERNAME}" --password-stdin
+                    docker push "\${params.DOCKER_IMAGE}"
+                    docker logout
+                    '''
+                }
+            }
         }
     }
 }
